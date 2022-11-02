1a853480ff5c00444ae50beaacc720331cf573d6
rezarayev <zaraev.rob@gmail.com>
2022-11-02 21:46:20 +0300

    fixup! CatBoost classes

--- src/CatBoost/utils/CB_data_preparation.py
+++ src/CatBoost/utils/CB_data_preparation.py
@@ -22,15 +22,11 @@ class DataPrepare():
         self.df['is_return'].replace({'N': 1, 'Y': 2}, inplace=True)
         self.df['mailtype'] = self.df['mailtype'].astype(int)
         self.df['mailctg'] = self.df['mailctg'].astype(int)
-        self.df.drop('mailrank', axis=1, inplace=True)
         self.df['directctg'] = self.df['directctg'].astype(int)
         self.df['postmark'] = self.df['postmark'].astype(int)
-        self.df.drop('name_mfi', axis=1, inplace=True)
         self.df['total_qty_over_index'] = self.df['total_qty_over_index'].astype('int')
-        drop_columns = [
-            'is_in_yandex', 'dist_qty_oper_login_1', 'total_qty_oper_login_1', 'total_qty_oper_login_0',
-            'total_qty_over_index_and_type', 'is_privatecategory', 'index_oper'
-        ]
+        drop_columns = ['is_in_yandex', 'dist_qty_oper_login_1', 'total_qty_oper_login_1', 'total_qty_oper_login_0',
+                        'total_qty_over_index_and_type', 'is_privatecategory', 'index_oper', 'name_mfi', 'mailrank']
         self.df.drop(drop_columns, axis=1, inplace=True)
         numeric_col = ['weight', 'transport_pay', 'weight_mfi', 'price_mfi', 'total_qty_over_index']
         for col in numeric_col:
